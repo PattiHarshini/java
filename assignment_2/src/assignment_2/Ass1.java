25d0cbf27ca7d4cfad4b3e368d07800f73a49ec9
PattiHarshini <pattiharshini1165@gmail.com>
2025-05-21 17:52:09 +0530

    second assignment

---
+++ assignment_2/src/assignment_2/Ass1.java
@@ -0,0 +1,18 @@
+package assignment_2;
+
+public class Ass1 {
+
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=Integer.parseInt(args[2]);
+		int d=Integer.parseInt(args[3]);
+		System.out.println(args[3]);
+		try {
+			System.out.println(args[6]);
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("The mentioned index is out of bounds");
+		}
+	}
+
+}
