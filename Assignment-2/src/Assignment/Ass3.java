8f21796e00995fc1a271dadd3e14c9aaee6881d9
PattiHarshini <pattiharshini1165@gmail.com>
2025-05-22 10:40:53 +0530

    java assignment 2

---
+++ Assignment-2/src/Assignment/Ass3.java
@@ -0,0 +1,26 @@
+package Assignment;
+import java.util.*;
+public class Ass3 {
+
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		try {
+		System.out.println("Enter the value of 'a':");
+		int a=sc.nextInt();
+		
+		System.out.println("Enter the value of 'b':");
+		int b=sc.nextInt();
+		int c=a/b;
+		System.out.println("result of a/b:"+c);
+		}catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+			
+
+		}catch(InputMismatchException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Please enter valid integer values");
+		}
+
+	}
+
+}
